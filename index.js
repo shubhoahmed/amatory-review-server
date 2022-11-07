29ecb3add676b1c8ad66536d379e532adb996ed3
shubhoahmed <shubhoahmed03@gmail.com>
2022-11-08 00:07:45 +0600

    mongo db added

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.snnxp9j.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Amatory Review server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Amatory Review server running on ${port}`);
+})
